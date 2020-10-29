de8047653727292bbfb8feac6d03e8de8cdce177
vivek <vivekjha419@yahoo.com>
2020-10-29 13:14:35 +0530

    todo1

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import store from './component/store'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
